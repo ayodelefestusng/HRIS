47cdbde307116e9e8be3b488350428df18ece47b
Ayodele Adeyinka <ayodelefestusng@gmail.com>
2026-03-02 13:39:01 +0100

    To tun again dd

--- myproject/settings.py
+++ myproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "*").split(",")
+# ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "*").split(",")
 # Application definition
 # 1. Allow the domain to serve the app
 ALLOWED_HOSTS = [
